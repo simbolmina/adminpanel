b598c89d7c03100ac47a6d3f67d0aed98617bb90
simbolmina <simbolmina@hotmail.com>
2023-07-14 23:18:47 +0300

    initial

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>adminpanel</span>
+      <span>© Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
